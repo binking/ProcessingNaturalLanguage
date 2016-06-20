d9b55898ca89cc4ef8b4b9c6b220e6964319eb09
江智彬 <jiangzhibin2014.xujie@gmail.com>
2016-06-20 17:37:16 +0800

    Upload stemmer and EditDistance directories

---
+++ EditDistance/EditDistance.py
@@ -0,0 +1,36 @@
+def EditDist(a, b):
+    # Stopping condition
+    if len(a) == 0:
+        return len(b)
+    if len(b) == 0:
+        return len(a)
+    delta = 1 if a[-1] != b[-1] else 0
+    return min( EditDist(a[:-1], b[:-1]) + delta,
+                EditDist(a, b[:-1]) + 1,
+                EditDist(a[:-1], b) + 1)
+
+def LevenshteinMethod(x, y):
+    matrix = []
+    for i in range(len(x)+1):
+        matrix.append([0] * (len(y)+1)) # initialize the matrix with zeros
+    for i in range((len(x) +1 )):
+        matrix[i][0] = i # Fill in the first column with acscending integers
+    for j in range((len(y) + 1)):
+        matrix[0][j] = j # Fill in the first row with ascending integers
+    for i in range(1, len(x) + 1 ):
+        for j in range(1, len(y) + 1):
+            # Fill in other elements
+            delta = 1 if x[i-1] != y[j-1] else 0
+            distDiag = matrix[i-1][j-1] + delta
+            distVer = matrix[i-1][j] + 1
+            distHor = matrix[i][j-1] + 1
+            matrix[i][j] = min(distDiag, distHor, distVer)
+    return matrix[-1][-1]
+
+import datetime as d
+#st = d.datetime.now()
+#print(EditDist("Shakespeare", "shake spear"))
+#print((d.datetime.now() - st).total_seconds())
+st = d.datetime.now()
+print(LevenshteinMethod("Shakespeare", "shake spear"))
+print((d.datetime.now() - st).total_seconds())
---
+++ EditDistance/Nok_Nok.py
@@ -0,0 +1,46 @@
+import EditDistance as ed
+import unittest
+
+comment_score = {"totally correct":0, "almost right":1, "quite close":2, "a bit confusing":3, "very confusing":4}
+error_toast = "Mapping Error"
+class SpellingTestCase(unittest.TestCase):
+    def test_right(self):
+        targeted_score = comment_score["totally correct"]
+        assert ed.LevenshteinMethod("Jiang", "Jiang") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("Chibin", "Chibin") == targeted_score, error_toast
+    def test_almost_right(self):
+        targeted_score = comment_score["almost right"]
+        assert ed.LevenshteinMethod("flocinaucinihilipilification", "floccinaucinihilipilification") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("owll", "owl") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("pseudopseudohipoparathyroidism", "pseudopseudohypoparathyroidism") == targeted_score, error_toast
+    def test_quite_close(self):
+        targeted_score = comment_score["quite close"]
+        assert ed.LevenshteinMethod("ples", "please") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("reqird", "required") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("rnser", "answer") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("antidisestablishmentaraniasm", "antidisestablishmentarianism") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("wol", "owl") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("humuhumunukunukuapuaua‘a", "humuhumunukunukuapua‘a") == targeted_score, error_toast
+    def test_a_bit_confusing(self):
+        targeted_score = comment_score["a bit confusing"]
+        assert ed.LevenshteinMethod("plez", "please") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("cnoke", "knock") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("reqid", "required") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("pneumonoultramicroscopiccilikovolkanokoniosis", "pneumonoultramicroscopicsilicovolcanokoniosis") == targeted_score, error_toast
+    def test_very_confusing(self):
+        targeted_score = comment_score["very confusing"]
+        assert ed.LevenshteinMethod("mispeln", "misspelling") == targeted_score, error_toast
+        assert ed.LevenshteinMethod("mestipenk", "mistyping") == targeted_score, error_toast
+
+def classify_comment(rightspelling, *misspellings):
+    score_comment = {value:key for key, value in comment_score.items()}
+    for mis in misspellings:
+        edit_dist = ed.LevenshteinMethod(mis, rightspelling)
+        yield mis + "\t" +str(edit_dist) +"\t"+score_comment[edit_dist]
+
+if __name__=="__main__":
+    results = classify_comment("typo", "oooo", "opyt", "pyto", "typ", "typa", "typotypo")
+    for res in results:
+        print(res)
+    unittest.main()
+
